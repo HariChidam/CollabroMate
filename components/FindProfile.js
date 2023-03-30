f34a03a8e96778c92e8c1356bacf7ac66fa8ffda
HariChidam <hchidam@umich.edu>
2023-03-30 16:29:48 -0400

    dashboard

--- components/FindProfile.js
+++ components/FindProfile.js
@@ -1,60 +1,67 @@
-import React, {useEffect, useState} from 'react'
-import { useSupabaseClient } from '@supabase/auth-helpers-react'
+import React, { useEffect, useState } from 'react';
+import { useSupabaseClient } from '@supabase/auth-helpers-react';
 import Profile from './Profile';
+import Link from 'next/link';
 
-export default function FindProfile({user}) {
+export default function FindProfile({ user }) {
+  const supabase = useSupabaseClient();
+  const [avatarUrl, setAvatarUrl] = useState('');
+  const [profiles, setProfiles] = useState([]);
+  const [userProfile, setUserProfile] = useState({});
 
-    const supabase = useSupabaseClient()
-    const [email, setEmail] = useState('');
-    const [Firstname, setFirstname] = useState('');
-    const [Lastname, setLastname] = useState('');
-    const [Grade, setGrade] = useState('');
-    const [Class, setClass] = useState('');
-    const [GPA, setGPA] = useState('');
-    const [avatarUrl, setAvatarUrl] = useState(''); 
+  useEffect(() => {
+    const getProfiles = async () => {
+      const { data, error } = await supabase.from('Profiles').select('*');
+      console.log(data);
 
-    useEffect(() => {
-        const getProfile = async () => {
-            const { data, error } = await supabase.from('Profiles')
-            .select('*')
-            .eq('email', user.email);
+      // filter out the current user's profile
+      const filteredProfiles = data.filter(profile => profile.email !== user.email);
+      const userProfile = data.filter(profile => profile.email === user.email);
+      console.log(filteredProfiles);
+      console.log(userProfile);
 
-            console.log(data)
+      // set state for all profiles except the current user's profile
+      setProfiles(filteredProfiles);
+      setUserProfile(userProfile[0] || {});
+    };
 
-            const { data: blob, error: downloadError } = await supabase.storage.from('profiles').download(user.id)
-
-            console.log(blob)
-
-            const url = URL.createObjectURL(blob)
-            setAvatarUrl(url)
-
-            console.log(url)
-            
-            setEmail(data[0].email);
-            setFirstname(data[0].Firstname);
-            setLastname(data[0].Lastname);
-            setGrade(data[0].Grade);
-            setClass(data[0].Class);
-            setGPA(data[0].GPA);
-            console.log(error);
-        }
-        getProfile();
-    },[supabase, user.email, user.id])
+    getProfiles();
+  }, [supabase, user.email]);
 
   return (
     <div>
-
-        {
-          avatarUrl ?
-
-          (
-            <Profile email={email} Firstname={Firstname} Lastname={Lastname} Grade={Grade} Class={Class} GPA={GPA} Url={avatarUrl}/>
-          )
-          :
-          (
-            <Profile email={email} Firstname={Firstname} Lastname={Lastname} Grade={Grade} Class={Class} GPA={GPA}/>
-          )
-        }
+      <div>
+        <h1 className="text-2xl font-bold mb-4">Your Profile</h1>
+        <div class="mx-auto w-2/6">
+          <Profile
+            key={userProfile.id}
+            email={userProfile.email}
+            Firstname={userProfile.Firstname}
+            Lastname={userProfile.Lastname}
+            Grade={userProfile.Grade}
+            Class={userProfile.Class}
+            GPA={userProfile.GPA}
+          />
+      </div>
     </div>
-  )
+      <div>
+        <h1 className="text-2xl font-bold my-4">Students</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {profiles.map(profile => (
+            <Profile
+              key={profile.id}
+              email={profile.email}
+              Firstname={profile.Firstname}
+              Lastname={profile.Lastname}
+              Grade={profile.Grade}
+              Class={profile.Class}
+              GPA={profile.GPA}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
+// https://vgtnsiusikeqlnnjzith.supabase.co/storage/v1/object/public/profiles/eee2092e-555c-4fae-bf6e-f974c9e0c9f1
+// https://vgtnsiusikeqlnnjzith.supabase.co/storage/v1/object/profiles/eee2092e-555c-4fae-bf6e-f974c9e0c9f1.jepg
--- components/Profile.js
+++ components/Profile.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Image from 'next/image'
+import Hari from '../public/Hari.JPG'
 
 export default function Profile({email, Firstname, Lastname, Grade, Class, GPA, Url}) {
   return (
     <div className="bg-white shadow rounded-lg p-6">
-      <h1 className="text-2xl font-bold mb-4">Profile</h1>
-      {Url && <Image src={Url} alt='profile pic'> </Image>}
-      <h2 className="text-lg font-medium mb-2">{Firstname + " " + Lastname}</h2>
+      <h1 className="text-2xl font-bold mb-4">{Firstname + " " + Lastname}</h1>
+      {/* {Url && <Image src={Url} alt='profile pic'> </Image>} */}
+      <Image src={Hari} alt='profile pic' width={200} height={200} className="rounded-full mb-4"></Image>
       <h2 className="text-gray-600 text-lg mb-2">{email}</h2>
       <div className="flex items-center mb-4">
         <span className="text-gray-600 text-lg font-medium mr-2">Grade:</span>
